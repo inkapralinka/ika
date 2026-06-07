6df7a1801ea648cc3de8219044748ed8fa7d2290
Inka <inka.garbiec777@gmail.com>
2026-06-07 13:41:03 +0200

    ulepszone liczby

--- liczby.py
+++ liczby.py
@@ -4,4 +4,14 @@ for i in range(16,2,-2):
 print("*" * 20)
 
 for j in range(1,7):
-        print(j)
+    print(j)
+
+print("*" * 20)
+
+for k in range(6,10):
+    print(k)
+
+print("*" * 20)
+ 
+for l in{6,9,6,9,6,9}:
+    print(l)
